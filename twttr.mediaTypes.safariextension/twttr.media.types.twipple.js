ec030bf84d2d1019951b3742b66e301f18fb9c0c
Norio Nomura <norio.nomura@gmail.com>
2011-01-27 09:41:51 +0900

    add p.twipple.jp support.

---
+++ twttr.mediaTypes.safariextension/twttr.media.types.twipple.js
@@ -0,0 +1,47 @@
+/*
+
+The MIT License
+
+Copyright (c) 2010-2011 Norio Nomura
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
+(function () {
+    if (typeof(twttr.mediaType) !== 'undefined'){
+        if (typeof(twttr.media.types.twipple) === 'undefined') {
+            twttr.mediaType('twttr.media.types.twipple', {
+                icon :'photo',
+                domain : 'http://p.twipple.jp/', matchers: {
+                    standardUrl: /^#{optional_protocol}?p\.twipple\.jp\/([^\/]+)\/?$/g
+                },
+                process : function (A) {
+                    this.data.id = this.slug;
+                    this.data.path = this.slug.match(/./g).join('/');
+                    A();
+                },
+                render : function(B){
+                    var A = '<div class="twipple"><a class="inline-media-image" data-inline-type="twipple" href="http://p.twipple.jp/{id}" target="_blank"><img src="http://p.twipple.jp/data/{path}.jpg"/></a></div>';
+                    $(B).append(twttr.util.supplant(A, this.data));
+                }
+            });
+        }
+    }
+})();
